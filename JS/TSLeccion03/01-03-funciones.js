655f9a50ff3905744fce19e3f1b4ae844f28ab68
Marianela <135768183+MarianelaPurretta@users.noreply.github.com>
2024-04-24 19:43:58 -0300

    JAVAClase4-7-1

---
+++ JS/TSLeccion03/01-03-funciones.js
@@ -0,0 +1,9 @@
+//Llamado a la funcion antes de definirla (hosting):
+miFuncion(8, 2)
+
+//Definimos una funcion
+function miFuncion(a, b) { //puede o no tener parametros o argumentos
+    console.log("Sumamos: " + (a + b))
+}
+//Llamamos la funcion
+miFuncion(5, 4)
