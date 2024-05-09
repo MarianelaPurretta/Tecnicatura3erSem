b6ff52412a82718a9de1c8e7dafedde586e11d6d
Marianela <135768183+MarianelaPurretta@users.noreply.github.com>
2024-05-09 17:19:27 -0300

    While ARREGLADO

--- ASISTENCIAS/practicas/script.js
+++ ASISTENCIAS/practicas/script.js
@@ -28,31 +28,26 @@ let perdidas = 0
 while (triunfos < 3 && perdidas < 3) {
     pc = aleatorio(3, 1)
     jugador = prompt("Elije: 1 piedra, 2 papel, 3 tijera")
+    //alert("Elije jugador: "+jugador)
+    alert("Pc elige: " + eleccion(pc))
+    alert("Tu eliges: " + eleccion(jugador))
+
+    // Combate
+    if (pc == jugador) {
+        alert("EMPATE")
+    } else if (jugador == 1 && pc == 3) {
+        alert("Ganaste")
+        triunfos = triunfos + 1
+    } else if (jugador == 2 && pc == 1) {
+        alert("Ganaste")
+        triunfos = triunfos + 1
+    } else if (jugador == 3 && pc == 2) {
+        alert("Ganaste")
+        triunfos = triunfos + 1
+    } else {
+        alert("Perdiste")
+        perdidas = perdidas + 1
+    }
 }
-
-
-
-
-
-//alert("Elije jugador: "+jugador)
-alert("Pc elige: " + eleccion(pc))
-alert("Tu eliges: " + eleccion(jugador))
-
-// Combate
-if (pc == jugador) {
-    alert("EMPATE")
-} else if (jugador == 1 && pc == 3) {
-    alert("Ganaste")
-    triunfos = triunfos + 1
-} else if (jugador == 2 && pc == 1) {
-    alert("Ganaste")
-    triunfos = triunfos + 1
-} else if (jugador == 3 && pc == 2) {
-    alert("Ganaste")
-    triunfos = triunfos + 1
-} else {
-    alert("Perdiste")
-    perdidas = perdidas + 1
-}
-
 alert("Ganaste " + triunfos + " veces. Perdiste " + perdidas + " veces.")
+
