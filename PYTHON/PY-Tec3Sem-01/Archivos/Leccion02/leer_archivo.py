eca44bc22c14dd29c078b7e50532cbc8b738acda
Marianela <135768183+MarianelaPurretta@users.noreply.github.com>
2024-05-13 22:47:50 -0300

    py|ARCHIVOS

---
+++ PYTHON/PY-Tec3Sem-01/Archivos/Leccion02/leer_archivo.py
@@ -0,0 +1,22 @@
+# modo para leer:
+# 1 read :'r'
+
+archivo = open('prueba.txt', 'r', encoding='utf8') #en los () especificamos la ruta del archivo: C:\\
+# print(archivo.read())
+# print(archivo.read(16)) #Muestra las primeras(n)letras del texto
+# print(archivo.readline())
+# print(archivo.readline())
+
+# Iteramos el archivo:
+# for linea in archivo:
+    # print(linea)
+# Metodo que muestra una lista
+
+# ANEXAMOS INFORMACION, COPIAMOS A OTRO:
+
+archivo2 = open('prueba.txt', 'a', encoding='utf8')
+archivo2.write(archivo.read())
+archivo.close()
+archivo2.close()
+
+print('Se ha terminado el proceso de leer y copiar archivos')
---
+++ PYTHON/PY-Tec3Sem-01/Archivos/Leccion02/manejo_archivos.py
@@ -0,0 +1,17 @@
+# Para abrir un archivo primero declaramos una variable:
+try:
+    archivo = open('prueba.txt', 'w', encoding='utf8') #metodo "open" para abrir o crear archivos en caSo de que no exista
+    # 'w': write ||| usamos encoding=utf8 para poder visualizar los acentos en el texto
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write('como por ejemplo: acción, ejecución y producción\n')
+    archivo.write('Las letras son:\na: append  \nw: write  \nr: read  \nx: crear archivo especificado  \n')
+    archivo.write('t: para texto o text, \nb: para binario o texto, \nw+: lee y escribe, \nr+: lee y escribe igual a w\n')
+    archivo.write('Saludos a a todos\n')
+    archivo.write('Con esto terminamos')
+except Exception as e:
+    print(e)
+finally:
+    archivo.close() # SIEMPRE SE DEBE CERRAR CON .CLOSE()
+
+#archivo.write('Todo quedó perfecto') # Error: ya está cerrado el archivo ya no podemos trabajar el él!
