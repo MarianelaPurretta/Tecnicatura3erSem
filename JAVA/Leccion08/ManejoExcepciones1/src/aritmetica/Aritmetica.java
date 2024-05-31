c376d5ea99d95f53974f386c7ca3e253271f6979
Marianela <135768183+MarianelaPurretta@users.noreply.github.com>
2024-05-31 16:24:26 -0300

    Clase8|EXCEPCIONES|JAVA|

--- JAVA/Leccion08/ManejoExcepciones1/src/aritmetica/Aritmetica.java
+++ JAVA/Leccion08/ManejoExcepciones1/src/aritmetica/Aritmetica.java
@@ -6,8 +6,7 @@ import excepciones.OperacionException;
 public class Aritmetica {
     public static int division(int numerador, int denominador) throws OperacionException{
         if (denominador == 0){
-            throw new OperacionException("Division entre cero");
-                    
+            throw new OperacionException("Division entre cero");                    
         }
         return numerador / denominador;
     }
--- JAVA/Leccion08/ManejoExcepciones1/src/excepciones/OperacionException.java
+++ JAVA/Leccion08/ManejoExcepciones1/src/excepciones/OperacionException.java
@@ -2,7 +2,7 @@
 package excepciones;
 
 
-public class OperacionException extends Exception {
+public class OperacionException extends RuntimeException {
     public OperacionException(String mensaje){
         super(mensaje);
     }
